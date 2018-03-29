8d9b5dd8f884296416e41df5829a9a7dafd3433a
DawsonXin <18508165054@163.com>
2018-03-29 22:28:04 +0800

    生产环境配置
    生产环境配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
 const path = require('path')
-const isDev = process.env.NODE_ENV ==='development'
-const webpack = require('webpack')
 const HTMLPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+const ExtractPlugin = require('extract-text-webpack-plugin')
+
+
+const isDev = process.env.NODE_ENV ==='development'
+
+
 let config = {
     target:"web",
     entry:path.join(__dirname,'src/index.js'),
     output:{
-        filename:'bundle.js',
+        filename:'bundle.[hash:8].js',
         path:path.join(__dirname,'dist')
     },
     module:{
@@ -19,27 +24,27 @@ let config = {
                 test:/\.jsx$/,
                 loader:'babel-loader'
             },
-            {
-                test:/\.css$/,
-                use:[
-                    'style-loader',
-                    'css-loader',
-                ]
-            },
-            {
-                test:/\.styl/,
-                use:[
-                    'style-loader',
-                    'css-loader',
-                    {
-                        loader:'postcss-loader',
-                        options:{
-                            sourceMap:true
-                        }
-                    },
-                    'stylus-loader'
-                ]
-            },
+            // {
+            //     test:/\.css$/,
+            //     use:[
+            //         'style-loader',
+            //         'css-loader',
+            //     ]
+            // },
+            // {
+            //     test:/\.styl/,
+            //     use:[
+            //         'style-loader',
+            //         'css-loader',
+            //         {
+            //             loader:'postcss-loader',
+            //             options:{
+            //                 sourceMap:true
+            //             }
+            //         },
+            //         'stylus-loader'
+            //     ]
+            // },
             {
                 test:/\.(gif|jpg|jpeg|png|svg)$/,
                 use:[
@@ -64,6 +69,22 @@ let config = {
     ]
 }
 if(isDev){
+    config.module.rules.push(
+        {
+            test:/\.styl/,
+            use:[
+                'style-loader',
+                'css-loader',
+                {
+                    loader:'postcss-loader',
+                    options:{
+                        sourceMap:true
+                    }
+                },
+                'stylus-loader'
+            ]
+        },
+    )
     config.devtool = '#cheap-module-eval-source-map'
     config.devServer = {
         port:8000,
@@ -81,5 +102,67 @@ if(isDev){
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoEmitOnErrorsPlugin()
     )
+}else{
+    config.entry = {
+        app:path.join(__dirname,'src/index.js'),
+        vendor:['vue']
+    }
+    config.output.filename = '[name].[chunkhash:8].js'
+    config.module.rules.push({
+        test:/\.styl/,
+        use:ExtractPlugin.extract({
+            fallback:'style-loader',
+            use:[
+                'css-loader',
+                {
+                    loader:'postcss-loader',
+                    options:{
+                        sourceMap:true
+                    }
+                },
+                'stylus-loader'
+            ]
+        })
+    })
+    config.plugins.push(
+        new ExtractPlugin('styles.[contentHash:8].css'),
+    )
+    config.optimization = {
+
+        splitChunks: {
+    
+          cacheGroups: {
+    
+            commons: {
+    
+              chunks: 'initial',
+    
+              minChunks: 2, maxInitialRequests: 5,
+    
+              minSize: 0
+    
+            },
+    
+            vendor: {
+    
+              test: /node_modules/,
+    
+              chunks: 'initial',
+    
+              name: 'vendor',
+    
+              priority: 10,
+    
+              enforce: true
+    
+            }
+    
+          }
+    
+        },
+    
+        runtimeChunk: true
+    
+      }
 }
 module.exports = config;
